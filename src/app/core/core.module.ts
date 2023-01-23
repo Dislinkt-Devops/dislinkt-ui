190923090f5b90d03bc769d6ee6f8b880cc93d5b
Mikec <mikecitt@gmail.com>
2023-01-23 19:11:40 +0100

    Inject headers on UI and intercept 401

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BaseUrlInterceptor } from './interceptor/base-url.interceptor';
-
-
+import { AuthInterceptor } from './interceptor/auth.interceptor';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: BaseUrlInterceptor,
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
---
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { catchError, Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const accessToken = this.authService.getUserInfo()?.accessToken;
+    if (accessToken) {
+      request = request.clone({
+        setHeaders: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+    }
+
+    return next.handle(request).pipe(
+      catchError(err => {
+        console.log(err);
+        if (err.status === 401) {
+          this.authService.logout();
+        }
+
+        const error = err.error.message || err.statusText;
+        throw new Error(error);
+      })
+    );
+  }
+}
